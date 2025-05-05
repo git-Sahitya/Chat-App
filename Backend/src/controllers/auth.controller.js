dac02bb28df4ac66ceb71e18044a78a94346176d
git-Sahitya <sahitya5289@gmail.com>
2025-05-05 17:20:45 +0530

    create  a route for userUpdateProfile and check the protected route

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { generateToken } from "../utils/utils.js";
+import cloudinary  from "../lib/cloudinary.js"
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -93,8 +94,25 @@ export const logout = async (req, res) => {
 
 export const updateProfile = async (req,res) => {
   try {
-    const {} = req.body
+    const {profilePic} = req.body
+    const userId = req.user._id
+    if (!profilePic) {
+      return res.status(400).json({message:"Profile pic is required."})
+    }
+    const uploadResponse =  await cloudinary.uploader.upload(profilePic)
+    const updatedUser = await userModel.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true})
+     res.status(200).json(updatedUser)
   } catch (error) {
-    
+    console.log("Error in update profile controllers:", error.message);
+    res.status(500).json({message: "Internal server error "})
   }
+}
+
+export  const checkAuth =async (req,res) => {
+   try {
+    res.status(200).json(req.user)
+   } catch (error) {
+    console.log("Error in Check controllers:", error.message);
+    res.status(500).json({message: "Internal server error "})
+   }
 }
