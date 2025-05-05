359679eacc3de271bbfc6b17f06d378e7b9630c8
git-Sahitya <sahitya5289@gmail.com>
2025-05-05 18:01:43 +0530

    implement message functionality with routes and controller methods

---
+++ Backend/src/controllers/message.controller.js
@@ -0,0 +1,59 @@
+import userModel from "../models/user.model.js"
+import messageModel from "../models/message.model.js"
+import cloudinary from "../lib/cloudinary.js"
+
+export const getUsersForSidebar = async (req,res) => {
+    try {
+        const loggedInUserId = req.user._id
+        const filteredUsers = await userModel.find({ _id : {$ne:loggedInUserId}}).select("-password")
+        res.status(200).json(filteredUsers)
+    } catch (error) {
+        console.log("Error in getUserForSidebar:", error.message);
+        res.status(500).json({message:"Internal server error"})
+        
+    }
+}
+
+export const getMessages = async (req,res) => {
+    try {
+        const {id:userToChatId} = req.params
+        const myId = req.user._id
+
+        const messages = await messageModel.find({
+            $or:[
+                { senderId:myId , receiverId:userToChatId},
+                {senderId:userToChatId , receiverId:myId}
+            ]
+        })
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log("Error in getMessages controller:", error.message);
+        res.status(500).json({message:"Internal server error"})
+    }
+}
+
+export const sendMessage = async (req,res) => {
+    try {
+        const {text,image} = req.body
+        const {id:receiverId} = req.params
+        const senderId = req.user._id
+
+        let imageUrl;
+        //Upload image to cloudinary
+        const uploadResponse = await cloudinary.uploader.upload(image)
+        imageUrl = uploadResponse.secure_url
+
+        const newMessage = new messageModel({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        })
+     await newMessage.save()
+     
+     res.status(200).json(newMessage)
+    } catch (error) {
+        console.log("Error in sendMessage controller:", error.message);
+        res.status(500).json({message:"Internal server error"})
+    }
+}
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 const app = express()
@@ -13,6 +14,7 @@ app.use(cookieParser())
 const PORT = process.env.PORT 
 
 app.use('/api/auth' , authRoutes)
+app.use('/api/message' , messageRoutes)
 
 
 app.listen(PORT,()=>{
